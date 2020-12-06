bbb1a450bbdf3b8c47376f2f1f1756bbe8a6a048
activedigital17 <active.digital17@gmail.com>
2020-12-06 10:35:13 +0100

    first commit

---
+++ app/src/main/java/com/aplantas/taskalica/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.aplantas.taskalica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // action baru stavlja property da se mjenja ime i dodaje back button
+        setupActionBarWithNavController(findNavController(R.id.navHostFragment))
+    }
+
+    // action baru stavlja property da se mjenja ime i dodaje back button
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.navHostFragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
